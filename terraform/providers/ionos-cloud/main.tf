b355a0057ba6933d7745e030f9eb1e174f88ce22
Safarveisi <sajad.safarveisi@gmail.com>
2025-02-28 16:48:36 +0100

    Modular provisioning

--- terraform/providers/ionos-cloud/main.tf
+++ terraform/providers/ionos-cloud/main.tf
@@ -2,25 +2,35 @@ terraform {
 
   backend "s3" {
     bucket = "customerintelligence"
-    key = "ml_platform/terraform.tfstate"
+    key    = "ml_platform/terraform.tfstate"
     region = "eu-central-1"
     endpoints = {
       s3 = "http://s3-de-central.profitbricks.com"
     }
     skip_credentials_validation = true
-    skip_requesting_account_id = true
+    skip_requesting_account_id  = true
   }
 
   required_providers {
     ionoscloud = {
-      source = "ionos-cloud/ionoscloud"
+      source  = "ionos-cloud/ionoscloud"
       version = ">= 6.4.10"
     }
   }
 }
 
 provider "ionoscloud" {
-    # For his authorization to work, 
-    # environment variable TF_VAR_ionos_token must be available 
-    token = "${var.ionos_token}" 
+  # For his authorization to work, 
+  # environment variable TF_VAR_ionos_token must be available 
+  token = "${var.ionos_token}"
+}
+
+
+module "kubeflow_cluster_1" {
+  source = "./kubeflow-cluster-module"
+
+  datacenter_id = "${var.datacenter_id}"
+  cluster_name  = "k8sDeveloperCluster"
+  k8s_version   = "1.31.3"
+  node_count    = 3
 }
--- terraform/providers/ionos-cloud/terraform.tfvars
+++
@@ -1,3 +0,0 @@
-cluster_name = "k8sDeveloperCluster"
-k8s_version = "1.31.3"
-node_count = 3

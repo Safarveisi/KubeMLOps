c7dc136dbfe27c608f3a1e7f512a48b860ff3188
Safarveisi <sajad.safarveisi@gmail.com>
2025-02-28 15:15:50 +0100

    Restructuring

--- terraform/providers/ionos-cloud/main.tf
+++ terraform/providers/ionos-cloud/main.tf
@@ -24,33 +24,3 @@ provider "ionoscloud" {
     # environment variable TF_VAR_ionos_token must be available 
     token = "${var.ionos_token}" 
 }
-
-resource "ionoscloud_k8s_cluster" "example" {
-  name                  = var.cluster_name
-  k8s_version           = var.k8s_version
-  maintenance_window {
-    day_of_the_week     = "Sunday"
-    time                = "09:00:00Z"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "ionoscloud_k8s_node_pool" "example" {
-  datacenter_id         = "${var.datacenter_id}"
-  k8s_cluster_id        = ionoscloud_k8s_cluster.example.id
-  name                  = "k8sNodePool"
-  k8s_version           = ionoscloud_k8s_cluster.example.k8s_version
-  maintenance_window {
-    day_of_the_week     = "Monday"
-    time                = "09:00:00Z"
-  } 
-  cpu_family            = "INTEL_SKYLAKE"
-  availability_zone     = "AUTO"
-  storage_type          = "SSD"
-  node_count            = var.node_count > 3 ? 3 : var.node_count
-  cores_count           = 8
-  ram_size              = 30720
-  storage_size          = 100
-}
